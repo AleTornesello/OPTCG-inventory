83ac3d4af21bdc022307083f490250c73c72940f
AleTornesello <alessandro.tornesello99@gmail.com>
2024-09-14 12:07:58 +0200

    feat: create card overlay with name, key and actions

--- src/app/cards/components/card-preview/card-preview.component.ts
+++ src/app/cards/components/card-preview/card-preview.component.ts
@@ -1,16 +1,25 @@
 import {Component, Input} from '@angular/core';
 import {CardModel} from "../../models/card.model";
 import {CardsListService} from "../../services/cards-list.service";
+import {ButtonComponent} from "../../../shared/components/button/button.component";
+import {faPlus, faMinus} from "@fortawesome/free-solid-svg-icons";
+import {InputTextComponent} from "../../../shared/components/inputs/input-text/input-text.component";
 
 @Component({
   selector: 'app-card-preview',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    InputTextComponent
+  ],
   templateUrl: './card-preview.component.html',
   styleUrl: './card-preview.component.scss'
 })
 export class CardPreviewComponent {
   @Input({required: true}) card!: CardModel;
+
+  public readonly faPlus = faPlus;
+  public readonly faMinus = faMinus;
 
   constructor(
     private _cardsListService: CardsListService
